5e3a3370c77452787787b9c24b33b0223af2ae8a
Wellington Lopes <wellyofgod@gmail.com>
2021-11-24 17:05:18 -0300

    Including API settings

--- config/services.php
+++ config/services.php
@@ -30,4 +30,9 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'brasilio' => [
+        'token' => env('BRASILIO_TOKEN'),
+        'url' => env('BRASILIO_URL'),
+    ]
+
 ];
