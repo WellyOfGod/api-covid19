c1f133a941d2cd0d69f2739a797315f49d554157
Wellington Lopes <wellyofgod@gmail.com>
2021-11-24 17:08:36 -0300

    Setting up routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Covid19Controller;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('cases-by-state-period', [Covid19Controller::class, 'casesByStatePeriod']);
