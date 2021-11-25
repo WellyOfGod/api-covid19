a8b0c07a67bf9963d5f883de614f4a8caf4b3265
Wellington Lopes <wellyofgod@gmail.com>
2021-11-25 15:11:36 -0300

    Parameterizing Top10State controller route

--- routes/api.php
+++ routes/api.php
@@ -4,15 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Covid19Controller;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::get('cases-by-state-period', [Covid19Controller::class, 'casesByStateDate']);
 
-Route::get('cases-by-state-period', [Covid19Controller::class, 'casesByStatePeriod']);
+Route::post('top10state',[Covid19Controller::class, 'top10state']);
